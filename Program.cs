c4f2dcfec188caf7ab1b2d8e58e1312e44f87e47
Venkataramana Chakravarthula <vchakravarthula@gmail.com>
2026-03-12 16:22:12 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            var sol = new Arrays.TwoSumSolution();
+
+            Console.WriteLine(string.Join(", ", sol.TwoSum(new int[] { 2, 7, 11, 15 }, 9)));
         }
     }
 }
